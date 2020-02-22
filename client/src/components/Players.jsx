90002557df27d8cffa19a5057dd18161ff532efd
squarerobin <squarerobin.82@gmail.com>
2020-02-22 02:48:24 +0100

    data is displayed

---
+++ client/src/components/Players.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import "./Players.css"
+import axios from 'axios';
+
+
+class Players extends React.Component {
+
+    constructor(){
+
+        super()
+        this.state = { 
+            
+            players : []
+         
+        }
+
+    }
+
+    componentDidMount() {
+        axios
+        .get("http://localhost:5000/api/players")
+          .then(res => {
+              this.setState({
+                players: res.data
+              })
+                
+              console.log(res.data)
+          })
+          //.then(players => this.setState({ women: playes.message }))
+          .catch(err => console.log("noooo"));
+          
+      }
+    render() {
+        return (
+
+
+            <div>
+{
+
+    this.state.players.map(player => {
+        return (
+
+
+            <div className="players-container">
+                <p>Name: {player.name}</p>
+                <p>Country: {player.country}</p>
+                <p>Searches: {player.searches}</p>
+
+            </div>
+
+        )
+
+    })
+
+}
+
+
+            </div>
+
+        )
+    }
+
+
+
+
+} 
+
+
+
+
+
+
+export default Players
