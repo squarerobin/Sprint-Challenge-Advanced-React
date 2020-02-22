4b8ea6fe33b8dce4318217e60cebd2a6c56d53de
squarerobin <squarerobin.82@gmail.com>
2020-02-22 03:05:51 +0100

    hooks done

--- client/src/components/Players.jsx
+++ client/src/components/Players.jsx
@@ -1,71 +1,85 @@
 import React from 'react';
 import "./Players.css"
 import axios from 'axios';
+import NavBar from "./Navbar"
+import { useDarkMode } from "../hooks/useDarkMode"
+
 
 
 class Players extends React.Component {
 
-    constructor(){
+    constructor() {
 
         super()
-        this.state = { 
-            
-            players : []
-         
+        this.state = {
+
+            players: []
+
         }
 
     }
 
     componentDidMount() {
         axios
-        .get("http://localhost:5000/api/players")
-          .then(res => {
-              this.setState({
-                players: res.data
-              })
-                
-              console.log(res.data)
-          })
-          //.then(players => this.setState({ women: playes.message }))
-          .catch(err => console.log("noooo"));
-          
-      }
-    render() {
-        return (
+            .get("http://localhost:5000/api/players")
+            .then(res => {
+                this.setState({
+                    players: res.data
+                })
 
+                console.log(res.data)
+            })
+            //.then(players => this.setState({ women: playes.message }))
+            .catch(err => console.log("noooo"));
 
-            <div>
-{
-
-    this.state.players.map(player => {
-        return (
-
-
-            <div className="players-container">
-                <p>Name: {player.name}</p>
-                <p>Country: {player.country}</p>
-                <p>Searches: {player.searches}</p>
-
-            </div>
-
-        )
-
-    })
-
-}
-
-
-            </div>
-
-        )
     }
 
 
 
 
-} 
 
 
+    render() {
+
+
+        return (
+
+
+            <div className="players">
+
+                <NavBar />
+
+
+                {
+                    this.state.players.map(player => {
+                        return (
+
+
+                            <div className="players-container">
+                                <p>Name: {player.name}</p>
+                                <p>Country: {player.country}</p>
+                                <p>Searches: {player.searches}</p>
+
+                            </div>
+
+                        )
+
+                    })
+
+                }
+            </div>
+        )
+
+
+
+
+
+
+
+
+    }
+
+}
 
 
 
