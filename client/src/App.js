11894ca54ceda3fdbb99d31010af06e5a481d06d
squarerobin <squarerobin.82@gmail.com>
2020-02-22 03:44:31 +0100

    stretch done: 2 hooks implemented

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,6 @@ class App extends Component {
   }
 }
 
-render(<App />, document.getElementById("root"));
-
 
 
 export default App
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, queryByText, getByTestId, getByPlaceholderText } from "@testing-library/react";
+import "mutationobserver-shim";
+import App from "./App";
+import Players from "./components/Players"
+import NavBar from "./components/Navbar"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+
+test("renders App without crashing", () => {
+  render(<App />);
 });
+  
+it('renders "name" text', () => {
+  const { queryByText } = render(
+    <App />
+    );
+    const name = queryByText(/Name/i);
+  });
+  it('renders "country" text', () => {
+    const { queryByText } = render(
+      <App />
+      );
+      const country = queryByText(/Country/i);
+    });
+    it('renders "searches" text', () => {
+      const { queryByText } = render(
+        <App />
+        );
+        const searches = queryByText(/Searches/i);
+      });
+ 
+  
+  /* 
+ */
+
+/* it('input for first name required" text', () => {
+  const firstNameInput = render(
+    <App />);
+  expect(firstNameInput.getByPlaceholderText("bill")).toBeRequired();
+});
+
+it('input for last name required" text', () => {
+ const lastNameInput = render(<App />);
+   expect(lastNameInput.getByPlaceholderText("luo")).toBeRequired();
+
+});
+ */
